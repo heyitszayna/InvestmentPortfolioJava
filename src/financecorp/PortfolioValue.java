c453d33eb01d3ba9834b5c9367abf3f0f7c19b56
Zayna Wasma <126897499+heyitszayna@users.noreply.github.com>
2024-01-28 12:39:02 +0400

    Update PortfolioValue.java

--- src/financecorp/PortfolioValue.java
+++ src/financecorp/PortfolioValue.java
@@ -1,15 +1,10 @@
-package financecorp_g4;
+package financecorp;
 
-/*
- * CSC301 Assignment 1
- * Done by: Zayna Wasma (1084503)
- */
-
-/* interface holds the the abstract method used for calculating the the total
- * portfolio value of the customer. the method is called in the investment 
+/* interface holds the abstract method used for calculating the total
+ * portfolio value of the customer. The method is called in the investment 
  * classes.
  */
 
-public interface PortfolioValue_G4 {
+public interface PortfolioValue {
     double calculateTotalPortfolioValue();
 } // end of interface
