e2b8d949f7d60b7132229d51b05d68e651f10856
Zayna Wasma <126897499+heyitszayna@users.noreply.github.com>
2024-01-28 12:39:36 +0400

    Update Stock.java

--- src/financecorp/Stock.java
+++ src/financecorp/Stock.java
@@ -1,15 +1,10 @@
-package financecorp_g4;
+package financecorp;
 
-/*
- * CSC301 Assignment 1
- * Done by: Zayna Wasma (1084503)
- */
-
-public class Stock_G4 extends Investment_G4 implements PortfolioValue_G4{
+public class Stock extends Investment implements PortfolioValue{
     private int dividendYield;
     private int marketCapitalization;
     
-    public Stock_G4(String customerName, String investmentType, String symbol, String name, int quantity, int purchasePrice, int currentMarketValue, int dividendYield, int marketCapitalization) {
+    public Stock(String customerName, String investmentType, String symbol, String name, int quantity, int purchasePrice, int currentMarketValue, int dividendYield, int marketCapitalization) {
         super(customerName, investmentType, symbol, name, quantity, purchasePrice, currentMarketValue);
         this.dividendYield = dividendYield;
         this.marketCapitalization = marketCapitalization;
@@ -44,7 +39,7 @@ public class Stock_G4 extends Investment_G4 implements PortfolioValue_G4{
                "\t marketCapitalization = " + getMarketCapitalization();
     }
     
-    /* Method is overriden from the interface. get;s the current market value
+    /* Method is overridden from the interface. Get's the current market value
     * of the the stock object.
     */
     
